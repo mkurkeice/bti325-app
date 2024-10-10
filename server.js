b8922c5d80631847c76e5322ebf87fdd03769d38
Manualla Kurkeice <mkurkeice@myseneca.ca>
2024-10-09 23:07:07 -0400

    server.js changes

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express'); //express module
-const path = require('path');
 const app = express();
 
 const HTTP_PORT = process.env.PORT || 8080; //server listening on PORT 8080
 
-app.use(express.static(path.join(__dirname, 'public'))); //static middleware to return /css/main.css
+app.use(express.static('public')); //static middleware to return /css/main.css
 
-app.get('/about', (req, res) => { //redirect user to about.html
-    res.redirect('/views/about.html');
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/about.html')); 
+});
+
+app.get('/', (req, res) => { //redirect user to about.html
+    res.redirect('/views/about.html');    
 });
 
 
