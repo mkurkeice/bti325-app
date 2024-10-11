32cb4f86ec0935a336682ed42f5c59481c7f9869
Manualla Kurkeice <mkurkeice@myseneca.ca>
2024-10-11 17:12:28 -0400

    server.js and blog-service.js changes

--- blog-service.js
+++ blog-service.js
@@ -18,3 +18,61 @@ const getCategories = () => {
         });
     });
 }; */
+import fs from 'fs';
+
+let posts = [];
+let categories = [];
+
+export function initialize() {
+    const getAllPosts = new Promise((resolve, reject) => {
+        fs.readFile('./data/posts.json', 'utf8', (err, postData) => {
+            if (err) throw err;
+            console.log(data);
+            posts = JSON.parse(data)      
+        });
+    })
+
+    const getCategories = new Promise((resolve, reject) => {
+        fs.readFile('./data/categories.json', 'utf8', (err, categoryData) => {
+            if (err) throw err;
+            console.log(data);
+            categories = JSON.parse(data);
+        });
+    })
+
+    return new Promise((resolve, reject) => {
+        Promise.all([promise1, promise2]).then(() => {
+            resolve()
+        }).catch(err => {
+            reject(err)
+        });
+    });
+}
+
+export function getAllPosts(){
+    return new Promise((resolve, reject) => {
+        if(posts.length === 0){
+            reject('no results returned')
+        }
+        resolve(posts);
+    });
+}
+
+export function getPublishedPosts(){
+    return new Promise((resolve,  reject) =>{
+        const publishedPosts = posts.filter(post => post.published === true);
+        if (publishedPosts.length === 0) {
+            reject('no results returned');
+        }
+        resolve(publishedPosts);
+    });
+}
+
+export function getCategories() {
+    return new Promise((resolve, reject) => {
+        if (categories.length === 0){
+            reject('no results returned');
+        }
+        resolve(categories);
+    });
+}
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+/*********************************************************************************
+*  BTI325 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: __Manualla Kurkeice__ Student ID: __163009228__ Date: ___10/11/2024___
+*
+*  Online (Vercel) Link: ________________________________________________________
+*
+********************************************************************************/ 
+
+
 const express = require('express'); //express module
 const path = require('path');
 const app = express();
-let blogService = require('./blog-service') //require blog-service.js modulef
-
 const HTTP_PORT = process.env.PORT || 8080; //server listening on PORT 8080
+let blogService = require('./blog-service') //require blog-service.js modulef
 
 app.use(express.static('public')); //static middleware to return /css/main.css
 
@@ -16,20 +27,45 @@ app.get('/', (req, res) => { //redirect user to about.html
 });
 
 app.get('/blog', (req, res) => {
-    res.send('TODO: get all posts who have published==true');
+    blogService.getPublishedPosts()
+        .then(publishedPosts => {
+            res.json(publishedPosts); 
+        })
+        .catch(err =>{
+            res.send({message: err});
+        })
 });
 
 app.get('/posts', (req, res) => {
-    res.send('TODO: get all posts');
+    blogService.getAllPosts()
+        .then(posts => {
+            res.json(posts); 
+        })
+        .catch(err =>{
+            res.send({message: err});
+        })
 });
 
 app.get('/categories', (req, res) => {
-    res.send('TODO: get all categories');
+    blogService.getCategories()
+        .then(categories => {
+            res.json(categories);
+        })
+        .catch(err =>{
+            res.send({message: err});
+        })
 });
 
 app.get('*', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views/404.html')); // create a 404.html file
 });
 
-
-app.listen(HTTP_PORT, () => { console.log(`server listening on: ${HTTP_PORT}`) });
+blogService.initialize() //server starts if .json files successfully parse
+    .then(() => {
+        app.listen(HTTP_PORT, () => {
+            console.log(`Server listening on port ${HTTP_PORT}`); //initialization successful, server starts
+        });
+    })
+    .catch(err => {    
+        console.error(`Failed to initialize data: ${err}`); //log error, server does not start
+    });
